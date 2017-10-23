f4ab2f82790273a77ecddfaabe040c7f3f5c2033
HyunJin Lee <hjleesm@naver.com>
2017-10-23 09:59:46 +0900

    add search input(text, button)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
@@ -14,7 +15,8 @@ import { ViewTagsComponent } from './view-tags/view-tags.component';
     ViewTagsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.component.css']
 })
 export class SearchComponent implements OnInit {
+  searchWord = '';
 
   constructor() { }
 
