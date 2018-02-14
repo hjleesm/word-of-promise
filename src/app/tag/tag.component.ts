215e5391e89954aab5aa0c56e0098c5a958e74c2
HyunJin Lee <hjleesm@naver.com>
2018-02-14 15:16:58 +0900

    Added some features
    - Change cursor to pointer on tag and logo comps
    - Added enter key search trigger on text input
    - Added click search trigger feature on tag comp

--- src/app/tag/tag.component.ts
+++ src/app/tag/tag.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { SearchDataService } from '../search-data.service';
+import { PageService } from '../page.service';
 
 @Component({
   selector: 'app-tag',
@@ -9,12 +10,22 @@ import { SearchDataService } from '../search-data.service';
 export class TagComponent implements OnInit {
   @Input() tag;
 
-  constructor(private searchDataService: SearchDataService) { }
+  constructor(
+    private searchDataService: SearchDataService,
+    private pageService: PageService
+  ) { }
 
   ngOnInit() {
   }
 
   onTagClicked() {
-    this.searchDataService.searchByTag(this.tag);
+    if (this.pageService.getPage() === this.pageService.PAGES.result) {
+      this.searchDataService.searchByTag(this.tag);
+    } else {
+      this.pageService.movePage({
+        page: this.pageService.PAGES.result,
+        searchWord: this.tag
+      });
+    }
   }
 }
