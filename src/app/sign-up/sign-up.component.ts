4b556b0a9df58e18ae86a58859cb5883bdcf47e7
HyunJin Lee <hjleesm@naver.com>
2018-02-19 21:49:20 +0900

    Set password rule

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-sign-up',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sign-up.component.css']
 })
 export class SignUpComponent implements OnInit {
+  @Input() id = '';
+  @Input() password = '';
+  @Input() password_confirm = '';
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit() {
+    if (this.password.length < 8) {
+      alert('The password must have at least 8 characters!');
+      return;
+    }
+
+    if (this.password !== this.password_confirm) {
+      alert('Confirm password and password do not match!');
+      return;
+    }
+  }
 }
