c6833ac9e3fdf909b5b4d1692e220d8771552ab4
HyunJin Lee <hjleesm@naver.com>
2018-02-27 09:23:26 +0900

    Added moving signup page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PageService } from '../page.service';
 
 @Component({
   selector: 'app-login',
@@ -9,12 +10,18 @@ export class LoginComponent implements OnInit {
   @Input() id;
   @Input() password;
 
-  constructor() { }
+  constructor(private pageService: PageService) { }
 
   ngOnInit() {
   }
 
   onLogin() {
-    
+
   }
+
+  onCreate() {
+    this.pageService.movePage({page: this.pageService.PAGES.signUp});
+  }
+
+
 }
